5e367f4fafc49351aeff60c6be9d49611c079c1e
jaopretoLIndo <163136135+jaopretomessias@users.noreply.github.com>
2024-08-28 07:58:33 -0300

    java sc

---
+++ index.js
@@ -0,0 +1,39 @@
+var nome = "João";
+
+let idade = 25;
+
+const pi = 3.14;
+
+let a = 5;
+
+let b = 10;
+
+let soma = a + b;
+
+console.log (soma);
+
+let a ;
+
+let b 10;
+
+function multiplica(x,y) {
+    retun x * y;
+}
+
+let produto = multiplica (a,b);
+
+variável produto
+
+console.log (produto);
+
+let frutas = ['maçã','banana','cereja'];
+
+console.log (frutas [1]);
+
+let pessoa = {
+    primeiroNome:'João',
+    sobrenome: 'Pedro',
+    idade: 17
+};
+
+console.log(pessoa.primeiroNome);
